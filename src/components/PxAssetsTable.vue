f565367432418a82061c26c60fd846918fd37981
devtwice <roger.dev.twice@gmail.com>
2020-07-31 19:20:58 +0200

    First Project with Vue

---
+++ src/components/PxAssetsTable.vue
@@ -0,0 +1,163 @@
+<template>
+  <table>
+    <thead>
+      <tr class="bg-gray-100 border-b-2 border-gray-400">
+        <th></th>
+        <th :class="{ up: this.sortOrder === 1, down: this.sortOrder === -1 }">
+          <span class="underline cursor-pointer" @click="changeSortOrder"
+            >Ranking</span
+          >
+        </th>
+        <th>Nombre</th>
+        <th>Precio</th>
+        <th>Cap. de Mercado</th>
+        <th>Variación 24hs</th>
+        <td class="hidden sm:block">
+          <input
+            class="bg-gray-100 focus:outline-none border-b border-gray-400 py-2 px-4 block w-full appearance-none leading-normal"
+            id="filter"
+            placeholder="Buscar..."
+            type="text"
+            v-model="filter"
+          />
+        </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="a in filteredAssets"
+        :key="a.id"
+        class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
+      >
+        <td>
+          <img
+            class="w-6 h-6"
+            :src="
+              `https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`
+            "
+            :alt="a.name"
+          />
+        </td>
+        <td>
+          <b>#{{ a.rank }}</b>
+        </td>
+        <td>
+          <router-link
+            class="hover:underline text-green-600"
+            :to="{ name: 'coin-detail', params: { id: a.id } }"
+          >
+            <!--Router dinamico, usa los valores de router-->
+            {{ a.name }}
+          </router-link>
+          <small class="ml-1 text-gray-500">{{ a.symbol }}</small>
+        </td>
+        <td>{{ a.priceUsd | dollar }}</td>
+        <td>{{ a.marketCapUsd | dollar }}</td>
+        <td
+          :class="
+            a.changePercent24Hr.includes('-')
+              ? 'text-red-600'
+              : 'text-green-600'
+          "
+        >
+          {{ a.changePercent24Hr | percent }}
+        </td>
+        <td class="hidden sm:block">
+          <px-button @custom-click="goToCoin(a.id)"
+            ><span>Detalle</span></px-button
+          >
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import PxButton from "@/components/PxButton";
+export default {
+  name: "PxAssetsTable",
+  components: { PxButton },
+  data() {
+    return {
+      filter: "",
+      sortOrder: 1,
+    };
+  },
+  props: {
+    assets: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    filteredAssets() {
+      /*No es necesaria esta condicion,tanto filter como sort retornaran igual assets funcione o no */
+      // if (!this.filter) {
+      //   return this.assets;
+      // }
+
+      /* !importante , altOrder devuelve el orden alternativo(al reves de sort)
+      sortOrder,retorna 1, si es falso retorna -1(altorder),  const altOrder se usa para 
+      al invertir el orden, si this.sorOrder es -1, altOrder sea al reves, con changeSorOrder()
+      invertimos también el sortOrder(asi los valores quedan cambiados para ser inverso)
+      */
+      const altOrder = this.sortOrder === 1 ? -1 : 1;
+      //filtro por name y por symbol !Importante, Flitro por nombreo siglas
+      return this.assets
+        .filter(
+          (a) =>
+            a.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+            a.name.toLowerCase().includes(this.filter.toLowerCase())
+        ) //Ordenamos por el rank
+        .sort((a, b) => {
+          if (parseInt(a.rank) > parseInt(b.rank)) {
+            return this.sortOrder;
+          }
+          return altOrder;
+        });
+    },
+  },
+  methods: {
+    goToCoin(id) {
+      /* no es lo mismo que $route, $router lo usamos para navegar a traves de codigo */
+      this.$router.push({ name: "coin-detail", params: { id } });
+    },
+    changeSortOrder() {
+      this.sortOrder = this.sortOrder === 1 ? -1 : 1;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.up::before {
+  content: "👆";
+}
+
+.down::before {
+  content: "👇";
+}
+
+td {
+  padding: 20px 0px;
+  font-size: 0.6rem;
+  text-align: center;
+}
+
+th {
+  padding: 5px;
+  font-size: 0.6rem;
+}
+
+@media (min-width: 640px) {
+  td,
+  th {
+    padding: 20px;
+    font-size: 1rem;
+  }
+
+  th {
+    padding: 12px;
+  }
+}
+</style>
